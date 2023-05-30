fca5c10ea80c66599cd8beead0b9234ec3845d87
sanket-kumbhare <sanketkumbhare1511@gmail.com>
2023-05-30 16:25:48 +0530

    useEffect abnormal behaviour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import { useState } from "react";
-import Todos from "./Todos";
+import { useState, useEffect } from "react";
+
 function App() {
-  let [counter, setCounter] = useState(0);
-  let [todos, setTodos] = useState([
-    { id: 1, todo: "todo1" },
-    { id: 2, todo: "todo2" },
-    { id: 3, todo: "todo3" },
-  ]);
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      console.log(counter);
+      setCounter((previous) => previous + 1);
+    }, 1000);
+  }, [counter]);
   return (
-    <div className="App">
-      <Todos todos={todos} />
-      <h1>Counter : {counter}</h1>
-      <button
-        onClick={() => {
-          setCounter(counter+1);
-        }}
-      >
-        Click
-      </button>
+    <div>
+      <h1>{counter}</h1>
+      <button>Stop</button>
     </div>
   );
 }
